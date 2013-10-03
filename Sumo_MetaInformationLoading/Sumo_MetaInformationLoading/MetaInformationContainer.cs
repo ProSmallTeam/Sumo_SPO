d69c06e68c3224a8f6dbdb3e45a038ac4b1e65f7
Zverev_Pirates <pelmeshka80@yandex.ru>
2013-10-03 23:43:34 +0400

    Добавил проект MetaInformationLoading для извлечения метаинформации о книге по ISBN-у Выделил поля, для хранения метаинформации

---
+++ Sumo_MetaInformationLoading/Sumo_MetaInformationLoading/MetaInformationContainer.cs
@@ -0,0 +1,37 @@
+﻿namespace Sumo_MetaInformationLoading
+{
+    class MetaInformationContainer
+    {
+        private string ruTitle;
+        private string enTitle;
+
+        private string author;
+
+        private string translator;
+
+        private string internalID;
+        private string _ISBN;
+
+        private string language;
+
+        private string publishHouse;
+        private string publishYear;
+
+        private string pageCount;
+
+        private string format;
+
+        private string edition;
+
+        private string category;
+
+        private string series;
+
+        private string binding;
+
+        private string annotation;
+
+        private string[] usersComents;
+
+    }
+}
---
+++ Sumo_MetaInformationLoading/Sumo_MetaInformationLoading/MetaInformationLoading.cs
@@ -0,0 +1,7 @@
+﻿namespace Sumo_MetaInformationLoading
+{
+    public class MetaInformationLoading
+    {
+
+    }
+}
