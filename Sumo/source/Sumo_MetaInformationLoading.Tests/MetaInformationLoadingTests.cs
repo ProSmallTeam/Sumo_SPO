191eaeb6f24acc358d5d9502b87c7580ba376e12
ZverevPirates <pelmeshka80@yandex.ru>
2013-10-21 13:06:51 -0700

    1) Добавлен парсер для лабиринта 2) Добавлены тесты для Ozona

--- Sumo/source/Sumo_MetaInformationLoading.Tests/MetaInformationLoadingTests.cs
+++ Sumo/source/Sumo_MetaInformationLoading.Tests/MetaInformationLoadingTests.cs
@@ -1,95 +1,53 @@
 ﻿namespace Sumo_MetaInformationLoading.Tests
 {
-    using System.Diagnostics;
+    using System;
+    using System.Collections.Generic;
 
     using NUnit.Framework;
 
     using Sumo_MetaInformationLoading.Ozon;
 
-    //    using HtmlAgilityPack;
-
-    /// <summary>
-    /// Тестирование класса загрузки мета информации.
-    /// </summary>
     [TestFixture]
     public class MetaInformationLoadingTests
     {
+        public const string ValidIsbn = "978-5-7502-0413-7";
+        public const string InvalidIsbn = "Some crap";
+
         [Test]
-        public void LoadPageTextTest()
+        public void ValidIsbnTest()
         {
-            var a = OzonPageParser.Parse("978-5-91671-156-1");
-            Trace.WriteLine(a.ToString());
-            //            var htmlPage =
-            //                new MetaInformationLoading("C:\\Users\\Vaigard\\Учеба\\sumo\\ozon.ru\\пример страници с книгой - Книга HTML5 для веб-дизайнеров - купить книжку html5 для веб-дизайнеров от Кит Джереми в книжном интернет магазине OZON.ru с доставкой по выгодной цене.htm", LoadingMod.FormFile);
-            //            var doc = new HtmlDocument();
-            //            doc.LoadHtml(htmlPage.TextOfPage);
-            //
-            //            var title = doc.DocumentNode.SelectNodes("//h1[@itemprop=\"name\"]");
-            //            var author = doc.DocumentNode.SelectNodes("//p[@itemprop=\"author\"]/a");
-            //            var language = doc.DocumentNode.SelectNodes("//p[@itemprop=\"inLanguage\"]");
-            //            var publisher = doc.DocumentNode.SelectNodes("//p[@itemprop=\"publisher\"]/a");
-            //            var isbn = doc.DocumentNode.SelectNodes("//p[@itemprop=\"isbn\"]");
-            //            var pages = doc.DocumentNode.SelectNodes("//span[@itemprop=\"numberOfPages\"]");
-            //            var category = doc.DocumentNode.SelectNodes("//li[@class=\"prevLast\"]/a");
-            //            var annotation = doc.DocumentNode.SelectNodes("//div[@class=\"mDetail_SidePadding\"]/table/tbody/tr/td");
-            //            var comments = doc.DocumentNode.SelectNodes("");
-            //            var link = doc.DocumentNode.SelectNodes("");
-            //            var linkImage = doc.DocumentNode.SelectNodes("");
-            //
-            //            Assert.AreEqual("Кит Джереми", author[0].InnerText);
+            var goodContainer = OzonPageParser.Parse(ValidIsbn);
+            Assert.AreEqual(goodContainer.Author, "Питер Гастон");
+            Assert.AreEqual(goodContainer.PageCount, 272);
+            Assert.AreEqual(goodContainer.PublishYear, "2012");
+            Assert.AreEqual(goodContainer.InternalId, "8465610");
+            Assert.AreEqual(goodContainer.Languages, "Русский");
+            Assert.AreEqual(goodContainer.PublishHouse, "БХВ-Петербург");
+            Assert.AreEqual(goodContainer.RuTitle, "CSS3. Руководство разработчика");
+            Assert.AreEqual(goodContainer.Link, "http://www.ozon.ru/context/detail/id/8465610/");
+            Assert.AreEqual(goodContainer.ISBN, new List<string>
+                                                            {
+                                                                "978-5-7502-0413-7",
+                                                                "978-5-9775-0845-2"
+                                                            });
+            Assert.AreEqual(goodContainer.Сategories.chain, new List<string>
+                                                            {
+                                                                "Книги",
+                                                                "Нехудожественная литература",
+                                                                "Компьютерная литература",
+                                                                "Интернет и Web-страницы",
+                                                                "Web-мастеринг. Языки и инструменты",
+                                                                "ASP, Perl, CGI, Python и другие языки для разработки Web-сайтов",
+                                                                "CSS3. Руководство разработчика"
+                                                            });
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void InvalidIsbnTest()
+        {
+            var badContainer = OzonPageParser.Parse(InvalidIsbn);
         }
     }
 }
-
-
-/*
-private void start()
-        {
-            my_delegate = new add_text(add_text_method);
-            // В цикле обрабатываем странички с first_page по last_page (те, что указали в полях ввода)
-            for (int i = this.first_page - 1; i &lt; this.last_page; i++)
-            {
-                string content = getRequest(tb_url.Text + this.param_separator + "p=" + i);
-                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-                doc.LoadHtml(content);
-                // Получаем список ссылок на страницы с резюме
-                HtmlNodeCollection c = doc.DocumentNode.SelectNodes("//a[@class='newtitle']");
-                if (c != null)
-                {
-                    // Обрабатываем каждую страницу (парсим из нее выбранные данные)
-                    foreach (HtmlNode n in c)
-                    {
-                        if (n.Attributes["href"] != null)
-                        {
-                            // Загружаем страничку с резюме
-                            string u = main_url + n.Attributes["href"].Value;
-                            string res_cn = getRequest(u);
-                            // Парсим страничку с резюме
-                            HtmlAgilityPack.HtmlDocument d = new HtmlAgilityPack.HtmlDocument();
-                            d.LoadHtml(res_cn);
-                            // Выбираем ячейки с нужными данными
-                            HtmlNodeCollection pads = d.DocumentNode.SelectNodes("//td[@class='pad_resume']");
-                            if (pads != null)
-                            {
-                                // Нам нужны только четные ячейки
-                                int j = 1;
-                                while (j &lt; pads.Count) {
-                                    // Записываем текст из ячейки в RTB (убираем все лишнее через trim)
-                                    rtb_output.Invoke(my_delegate, new object[] { pads[j].InnerText.Trim() + ";" });
-                                    j = j + 2;
-                                }
-                                rtb_output.Invoke(my_delegate, new object[] { "\n" });
-                            }
-                        }
-                    }
-                }
-            }
-            a_delegate = new set_text(set_text_method);
-            // Убираем дубли
-            rtb_output.Invoke(a_delegate, new object[] { array_unique(rtb_output.Lines) });\
-            // Делаем элементы формы активными
-            tlp_main.Enabled = true;
-            // Завершаем поток
-            tr.Abort();
-        }
-*/
