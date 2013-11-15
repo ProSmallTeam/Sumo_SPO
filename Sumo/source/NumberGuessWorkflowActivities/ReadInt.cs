7081311456e38b42e119ccccc9f18da967357172
Danemano <danemano@gmail.com>
2013-11-15 23:45:00 +0400

    изучение WWF часть 1.

---
+++ Sumo/source/NumberGuessWorkflowActivities/ReadInt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace NumberGuessWorkflowActivities
+{
+
+    public sealed class ReadInt : NativeActivity<int>
+    {
+        [RequiredArgument]
+        public InArgument<string> BookmarkName { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(NativeActivityContext context)
+        {
+            string name = BookmarkName.Get(context);
+
+            if (name == string.Empty)
+            {
+                throw new ArgumentException("BookmarkName cannot be an Empty string.",
+                    "BookmarkName");
+            }
+
+            context.CreateBookmark(name, new BookmarkCallback(OnReadComplete));
+        }
+
+        // NativeActivity derived activities that do asynchronous operations by calling 
+        // one of the CreateBookmark overloads defined on System.Activities.NativeActivityContext 
+        // must override the CanInduceIdle property and return true.
+        protected override bool CanInduceIdle
+        {
+            get { return true; }
+        }
+
+        void OnReadComplete(NativeActivityContext context, Bookmark bookmark, object state)
+        {
+            this.Result.Set(context, Convert.ToInt32(state));
+        }
+    }
+}
