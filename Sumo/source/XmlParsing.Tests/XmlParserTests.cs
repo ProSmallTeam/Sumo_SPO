cbf6c2bd25087dd5de78795f98847349ca8cf6a9
Geraskevi4_Tombers <mr.vash@mail.ru>
2013-11-17 10:31:43 +0400

    Поправил тесты для парсинга xml
    также перенесес проект в папку source

--- Sumo/source/XmlParsing.Tests/XmlParserTests.cs
+++ Sumo/source/XmlParsing.Tests/XmlParserTests.cs
@@ -65,6 +65,7 @@ namespace XmlParsing.Tests
             var book = XmlParser.Parse(xml);
 
             Assert.AreEqual("1", book.Md5Hash);
+            Assert.IsNull(book.Name);
             Assert.AreEqual("3", book.Path);
             Assert.AreEqual("4", book.SecondaryFields["Authors"]);
             Assert.AreEqual("5", book.SecondaryFields["PicturePath"]);
@@ -100,6 +101,7 @@ namespace XmlParsing.Tests
 
             var book = XmlParser.Parse(xml);
 
+            Assert.IsNull(book.Md5Hash);
             Assert.AreEqual("2", book.Name);
             Assert.AreEqual("3", book.Path);
             Assert.AreEqual("4", book.SecondaryFields["Authors"]);
@@ -120,34 +122,13 @@ namespace XmlParsing.Tests
             var xml = @"<Book>
     <Name>2</Name>
 	<Path>3</Path>
-	<SecondaryFields>
-		<Authors>4</Authors>
-		<PicturePath>5</PicturePath>	
-		<FileFormat>6</FileFormat>	
-		<InternalId>7</InternalId>
-		<ISBN>8</ISBN>
-		<Language>9</Language>
-		<PublicHouse>10</PublicHouse>
-		<PublicYear>11</PublicYear>
-		<PageCount>12</PageCount>
-		<Categories>13</Categories>
-	</SecondaryFields>
 </Book>";
 
             var book = XmlParser.Parse(xml);
 
             Assert.AreEqual("2", book.Name);
             Assert.AreEqual("3", book.Path);
-            Assert.AreEqual("4", book.SecondaryFields["Authors"]);
-            Assert.AreEqual("5", book.SecondaryFields["PicturePath"]);
-            Assert.AreEqual("6", book.SecondaryFields["FileFormat"]);
-            Assert.AreEqual("7", book.SecondaryFields["InternalId"]);
-            Assert.AreEqual("8", book.SecondaryFields["ISBN"]);
-            Assert.AreEqual("9", book.SecondaryFields["Language"]);
-            Assert.AreEqual("10", book.SecondaryFields["PublicHouse"]);
-            Assert.AreEqual("11", book.SecondaryFields["PublicYear"]);
-            Assert.AreEqual("12", book.SecondaryFields["PageCount"]);
-            Assert.AreEqual("13", book.SecondaryFields["Categories"]);
+            Assert.AreEqual(0, book.SecondaryFields.Count);
         }
     }
 }
