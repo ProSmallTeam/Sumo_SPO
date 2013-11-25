0cb8bfcf99e8d61b273a3705d3c068d01641be44
Danemano <danemano@gmail.com>
2013-11-25 23:35:44 +0400

    Добавлен XmlBookConvert.csproj с тестами.

---
+++ Sumo/source/XmlBookConvert.Tests/XmlBookConverterTests.cs
@@ -0,0 +1,248 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Sumo.API;
+
+namespace XmlBookConvert.Tests
+{
+    [TestFixture]
+    public class XmlBookConverterTests
+    {
+        private Dictionary<string, List<string>> _secondaryFields;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _secondaryFields = new Dictionary<string, List<string>>
+            {
+                {"Authors", new List<string>(new[] {"4", "44", "444"})},
+                {"Language", new List<string>(new[] {"5"})},
+                {"ISBN", new List<string>(new[] {"6"})},
+                {"PublicYear", new List<string>(new[] {"7"})}
+            };
+        }
+
+        #region XmlToBookTests
+
+        [Test]
+        public void ParseFullXmlTest()
+        {
+            var xmlContent = @"<Book>	
+    <Md5Hash>1</Md5Hash>
+    <Name>2</Name>
+	<Path>3</Path>
+	<SecondaryFields>
+		<Authors>
+            <Author>4</Author>
+            <Author>44</Author>			
+        </Authors>
+		<PicturePath>5</PicturePath>	
+		<FileFormat>6</FileFormat>	
+		<InternalId>7</InternalId>
+		<ISBN>8</ISBN>
+		<Language>9</Language>
+		<PublicHouse>10</PublicHouse>
+		<PublicYear>11</PublicYear>
+		<PageCount>12</PageCount>
+		<Categories>13</Categories>
+	</SecondaryFields>
+</Book>";
+            var xDocument = XDocument.Parse(xmlContent);
+
+            var book = XmlBookConverter.XmlToBook(xDocument);
+
+            Assert.AreEqual("1", book.Md5Hash);
+            Assert.AreEqual("2", book.Name);
+            Assert.AreEqual("3", book.Path);
+            Assert.AreEqual("4", book.SecondaryFields["Authors"][0]);
+            Assert.AreEqual("44", book.SecondaryFields["Authors"][1]);
+            Assert.AreEqual("5", book.SecondaryFields["PicturePath"][0]);
+            Assert.AreEqual("6", book.SecondaryFields["FileFormat"][0]);
+            Assert.AreEqual("7", book.SecondaryFields["InternalId"][0]);
+            Assert.AreEqual("8", book.SecondaryFields["ISBN"][0]);
+            Assert.AreEqual("9", book.SecondaryFields["Language"][0]);
+            Assert.AreEqual("10", book.SecondaryFields["PublicHouse"][0]);
+            Assert.AreEqual("11", book.SecondaryFields["PublicYear"][0]);
+            Assert.AreEqual("12", book.SecondaryFields["PageCount"][0]);
+            Assert.AreEqual("13", book.SecondaryFields["Categories"][0]);
+
+            Assert.AreEqual(10, book.SecondaryFields.Count);
+        }
+
+        [Test]
+        public void ParseXmlWithoutNameTest()
+        {
+            var xmlContent = @"<Book>	
+    <Md5Hash>1</Md5Hash>
+	<Path>3</Path>
+	<SecondaryFields>
+		<Authors>
+            <Author>4</Author>
+            <Author>44</Author>			
+        </Authors>
+		<PicturePath>5</PicturePath>	
+		<FileFormat>6</FileFormat>	
+		<InternalId>7</InternalId>
+		<ISBN>8</ISBN>
+		<Language>9</Language>
+		<PublicHouse>10</PublicHouse>
+		<PublicYear>11</PublicYear>
+		<PageCount>12</PageCount>
+		<Categories>13</Categories>
+	</SecondaryFields>
+</Book>";
+
+            var xDocument = XDocument.Parse(xmlContent);
+
+            var book = XmlBookConverter.XmlToBook(xDocument);
+
+            Assert.AreEqual("1", book.Md5Hash);
+            Assert.IsNull(book.Name);
+            Assert.AreEqual("3", book.Path);
+            Assert.AreEqual("4", book.SecondaryFields["Authors"][0]);
+            Assert.AreEqual("44", book.SecondaryFields["Authors"][1]);
+            Assert.AreEqual("5", book.SecondaryFields["PicturePath"][0]);
+            Assert.AreEqual("6", book.SecondaryFields["FileFormat"][0]);
+            Assert.AreEqual("7", book.SecondaryFields["InternalId"][0]);
+            Assert.AreEqual("8", book.SecondaryFields["ISBN"][0]);
+            Assert.AreEqual("9", book.SecondaryFields["Language"][0]);
+            Assert.AreEqual("10", book.SecondaryFields["PublicHouse"][0]);
+            Assert.AreEqual("11", book.SecondaryFields["PublicYear"][0]);
+            Assert.AreEqual("12", book.SecondaryFields["PageCount"][0]);
+            Assert.AreEqual("13", book.SecondaryFields["Categories"][0]);
+        }
+
+        [Test]
+        public void ParseXmlWithoutMd5HashTest()
+        {
+            var xmlContent = @"<Book>
+    <Name>2</Name>
+	<Path>3</Path>
+	<SecondaryFields>
+		<Authors>
+            <Author>4</Author>
+            <Author>44</Author>			
+        </Authors>
+		<PicturePath>5</PicturePath>	
+		<FileFormat>6</FileFormat>	
+		<InternalId>7</InternalId>
+		<ISBN>8</ISBN>
+		<Language>9</Language>
+		<PublicHouse>10</PublicHouse>
+		<PublicYear>11</PublicYear>
+		<PageCount>12</PageCount>
+		<Categories>13</Categories>
+	</SecondaryFields>
+</Book>";
+
+            var xDocument = XDocument.Parse(xmlContent);
+
+            var book = XmlBookConverter.XmlToBook(xDocument);
+
+            Assert.IsNull(book.Md5Hash);
+            Assert.AreEqual("2", book.Name);
+            Assert.AreEqual("3", book.Path);
+            Assert.AreEqual("4", book.SecondaryFields["Authors"][0]);
+            Assert.AreEqual("44", book.SecondaryFields["Authors"][1]);
+            Assert.AreEqual("5", book.SecondaryFields["PicturePath"][0]);
+            Assert.AreEqual("6", book.SecondaryFields["FileFormat"][0]);
+            Assert.AreEqual("7", book.SecondaryFields["InternalId"][0]);
+            Assert.AreEqual("8", book.SecondaryFields["ISBN"][0]);
+            Assert.AreEqual("9", book.SecondaryFields["Language"][0]);
+            Assert.AreEqual("10", book.SecondaryFields["PublicHouse"][0]);
+            Assert.AreEqual("11", book.SecondaryFields["PublicYear"][0]);
+            Assert.AreEqual("12", book.SecondaryFields["PageCount"][0]);
+            Assert.AreEqual("13", book.SecondaryFields["Categories"][0]);
+        }
+
+        [Test]
+        public void ParseXmlWithoutSecondaryFieldsTest()
+        {
+            var xmlContent = @"<Book>
+    <Md5Hash>1</Md5Hash>
+    <Name>2</Name>
+	<Path>3</Path>
+</Book>";
+
+            var xDocument = XDocument.Parse(xmlContent);
+
+            var book = XmlBookConverter.XmlToBook(xDocument);
+
+            Assert.AreEqual("1", book.Md5Hash);
+            Assert.AreEqual("2", book.Name);
+            Assert.AreEqual("3", book.Path);
+            Assert.AreEqual(0, book.SecondaryFields.Count);
+        }
+
+        #endregion 
+
+        #region BookToXmlTests
+
+        [Test]
+        public void FullBookToXmlTest()
+        {
+            var book = new Book
+            {
+                Md5Hash = "1",
+                Name = "2",
+                Path = "3",
+                SecondaryFields = _secondaryFields
+            };
+
+            var xDocument = XmlBookConverter.BookToXml(book);
+
+            Trace.WriteLine(xDocument);
+        }
+
+        [Test]
+        public void BookWithoutNameToXmlTest()
+        {
+            var book = new Book
+            {
+                Md5Hash = "1",
+                Name = null,
+                Path = "3",
+                SecondaryFields = _secondaryFields
+            };
+
+            var xDocument = XmlBookConverter.BookToXml(book);
+
+            Trace.WriteLine(xDocument);
+        }
+
+        [Test]
+        public void BookWithoutMd5HashToXmlTest()
+        {
+            var book = new Book
+            {
+                Md5Hash = null,
+                Name = "2",
+                Path = "3",
+                SecondaryFields = _secondaryFields
+            };
+
+            var xDocument = XmlBookConverter.BookToXml(book);
+
+            Trace.WriteLine(xDocument);
+        }
+
+        [Test]
+        public void BookWithoutSecondaryFieldsToXmlTest()
+        {
+            var book = new Book
+            {
+                Md5Hash = "1",
+                Name = "2",
+                Path = "3",
+                SecondaryFields = null
+            };
+
+            var xDocument = XmlBookConverter.BookToXml(book);
+
+            Trace.WriteLine(xDocument);
+        }
+
+        #endregion
+    }
+}
---
+++ Sumo/source/XmlBookConvert/XmlBookConverter.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Sumo.API;
+
+namespace XmlBookConvert
+{
+    public static class XmlBookConverter
+    {
+        public static Book XmlToBook(XDocument xDocument)
+        {
+            var root = xDocument.Element("Book");
+            var secondaryFieldsElements = root.Element("SecondaryFields");
+
+            var secondaryFields = new Dictionary<string, List<string>>();
+
+            if (secondaryFieldsElements != null)
+                foreach (var element in secondaryFieldsElements.Elements())
+                {
+                    secondaryFields.Add(element.Name.ToString(),
+                        element.Elements().Any()
+                            ? element.Elements().Select(xElement => xElement.Value).ToList()
+                            : new List<string> { element.Value });
+                }
+
+
+            var book = new Book
+            {
+                Md5Hash = root.Element("Md5Hash") != null ? root.Element("Md5Hash").Value : null,
+                Name = root.Element("Name") != null ? root.Element("Name").Value : null,
+                Path = root.Element("Path") != null ? root.Element("Path").Value : null,
+                SecondaryFields = secondaryFields
+            };
+
+            return book;
+        }
+
+        public static XDocument BookToXml(Book book)
+        {
+            var md5HashElement = book.Md5Hash != null ? new XElement("Md5Hash", book.Md5Hash) : null;
+
+            var nameElement = book.Name != null ? new XElement("Name", book.Name) : null;
+
+            var pathElement = book.Path != null ? new XElement("Path", book.Path) : null;
+
+            var secondaryFieldsElement = book.SecondaryFields != null ? new XElement("SecondaryFields") : null;
+
+            if (book.SecondaryFields != null)
+            {
+                foreach (var field in book.SecondaryFields)
+                {
+                    if (field.Value.Count != 1)
+                    {
+                        var fieldElement = new XElement(field.Key);
+
+                        for (var i = 0; i < field.Value.Count; i++)
+                        {
+                            fieldElement.Add(new XElement("Item" + i, field.Value[i]));
+                        }
+
+                        secondaryFieldsElement.Add(fieldElement);
+                    }
+                    else
+                    {
+                        secondaryFieldsElement.Add(new XElement(field.Key, field.Value[0]));
+                    }
+                }
+            }
+
+            var bookElement = new XElement("Book", md5HashElement, nameElement, pathElement, secondaryFieldsElement);
+
+            return new XDocument(bookElement);
+        }
+    }
+}
