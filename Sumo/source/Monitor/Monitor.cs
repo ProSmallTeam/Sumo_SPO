efa125cb34f5433d39553f9ec84fda2edcae24d5
Danemano <danemano@gmail.com>
2013-11-25 22:50:02 +0400

    Добавлен Monitor.cs

---
+++ Sumo/source/Monitor/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using FileSystemObserver;
+using Sumo.API;
+
+namespace Monitor
+{
+    public class Monitor
+    {
+        private readonly IDbTaskManager _dbTaskManager;
+
+        private readonly IFileSystemObserver _fileSystemObserver;
+
+        public Monitor(IFileSystemObserver fileSystemObserver, IDbTaskManager dbTaskManager)
+        {
+            _fileSystemObserver = fileSystemObserver;
+            _dbTaskManager = dbTaskManager;
+
+            _fileSystemObserver.FoldersChanged += FoldersChanged;
+
+        }
+
+        public void Run()
+        {
+            _fileSystemObserver.Run();
+        }
+
+        void FoldersChanged(object sender, FileObserverEventArgs e)
+        {
+            _dbTaskManager.AddTasks(e.ChangedFilesFullPath);
+        }
+        
+
+    }
+}
