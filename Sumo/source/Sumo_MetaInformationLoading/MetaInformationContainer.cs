73c746fe51a209c664642b2476e9d1fbec44fff1
Danemano <danemano@gmail.com>
2013-11-25 22:00:10 +0400

    Revert "Изменена структура класса MetaLoader"
    This reverts commit b5b03b7a0b7bb86d8759768193471a66877c426e.

---
+++ Sumo/source/Sumo_MetaInformationLoading/MetaInformationContainer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace MetaInformationLoader
+{
+    using MetaInformationLoader.Ozon;
+
+    /// <summary>
+    /// Контейнер хранения метаинформации.
+    /// </summary>
+    public class MetaInformationContainer
+    {
+        /// <summary>
+        /// Получает ссылку на книгу.
+        /// </summary>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Получает ссылку на обложку книги.
+        /// </summary>
+        public string PictureLink { get; set; }
+
+        /// <summary>
+        /// Получает название книги на английском.
+        /// </summary>
+        public string EnTitle { get; set; }
+
+        /// <summary>
+        /// Получает название книги на русском.
+        /// </summary>
+        public string RuTitle { get; set; }
+
+        /// <summary>
+        /// Получает формат файла книги.
+        /// </summary>
+        public string FileFormat { get; private set; }
+
+        /// <summary>
+        /// Получает автора книги.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Получает внутренний ID книги на сайте.
+        /// </summary>
+        public string InternalId { get; set; }
+
+        /// <summary>
+        /// Получает ISBN книги.
+        /// </summary>
+        public List<string> ISBN { get; set; }
+
+        /// <summary>
+        /// Получает язык, на котором написана книга.
+        /// </summary>
+        public string Languages { get; set; }
+
+        /// <summary>
+        /// Получает издателя.
+        /// </summary>
+        public string PublishHouse { get; set; }
+
+        /// <summary>
+        /// Получает год издания книги.
+        /// </summary>
+        public string PublishYear { get; set; }
+
+        /// <summary>
+        /// Получает количество страниц в книге.
+        /// </summary>
+        public int PageCount { get; set; }
+
+        /// <summary>
+        /// Получает категорию.
+        /// </summary>
+        public OzonChainCategories Сategories { get; set; }
+
+        /// <summary>
+        /// Получает аннотацию к книге.
+        /// </summary>
+        public string Annotation { get; set; }
+
+        /// <summary>
+        /// Получает коментарии пользователей на книгу.
+        /// </summary>
+        public OzonComentsList UsersComents { get; set; }
+    }
+}
---
+++ Sumo/source/Sumo_MetaInformationLoading/PageLoader.cs
@@ -0,0 +1,49 @@
+﻿namespace MetaInformationLoader
+{
+    using System.IO;
+    using System.Net;
+    using System.Text;
+
+    /// <summary>
+    /// Класс, отвечающий за загрузку метаинформации о книге.
+    /// </summary>
+    internal static class PageLoader
+    {
+        /// <summary>
+        /// Метод загрузки текста страници по конкретному url.
+        /// </summary>
+        /// <param name="url">
+        /// Url страници.
+        /// </param>
+        /// <returns>
+        /// Текст загруженной страници.
+        /// </returns>
+        public static Page LoadFromUrl(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                var encoding = response.CharacterSet != null ? Encoding.GetEncoding(response.CharacterSet) : Encoding.UTF8;
+
+                using (var stream = response.GetResponseStream())
+                    return new Page(response.ResponseUri.AbsoluteUri, ReadTextFromStream(stream, encoding));
+            }
+        }
+
+        /// <summary>
+        /// Метод считывания текста страници из потока.
+        /// </summary>
+        /// <param name="stream">
+        ///     Поток для считывания.
+        /// </param>
+        /// <param name="encoding"></param>
+        /// <returns>
+        /// Текст загруженной страници.
+        /// </returns>
+        private static string ReadTextFromStream(Stream stream, Encoding encoding)
+        {
+            var reader = new StreamReader(stream, encoding);
+            return reader.ReadToEnd();
+        }
+    }
+}
