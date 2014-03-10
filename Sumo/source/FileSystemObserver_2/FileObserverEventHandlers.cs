49629728ef72bc9a5bc2e2b6ed945039f02c248e
Danemano <danemano@gmail.com>
2014-03-10 19:09:11 +0400

    Наведен порядок в репозитории

--- Sumo/source/FileSystemObserver_2/FileObserverEventHandlers.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace FileSystemObserver_2
-{
-    public delegate void FileObserverEventHandler(object sender, FileObserverEventArgs e);
-    public delegate void FileObserverRenamedEventHandler(object sender, FileObserverRenamedEventArgs e);
-}
