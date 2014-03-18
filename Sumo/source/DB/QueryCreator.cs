8c7d9f0300b1c9f92ac81c3eb5bc61cf39c59da5
IziTombers <izmalkovdi@gmail.com>
2014-03-19 01:36:24 +0400

    Исправлены ошибки связанные с удалением Collections.cs

--- Sumo/source/DB/QueryCreator.cs
+++ Sumo/source/DB/QueryCreator.cs
@@ -14,7 +14,7 @@ namespace DB
             var attributes = attributesQuery.Split(new[] { ", ", "{", "}" }, StringSplitOptions.RemoveEmptyEntries);
 
             var query = attributes.Select(attributeName => new QueryDocument(new BsonDocument {{ "Name", attributeName }}));
-            var queryResults = query.Select(queryDocument => Collections.Attributes.FindOneAs<BsonDocument>(queryDocument)).Where(document => document != null);
+            var queryResults = query.Select(queryDocument => DataBase.Attributes.FindOneAs<BsonDocument>(queryDocument)).Where(document => document != null);
             return queryResults.Select(attr => int.Parse(attr["_id"].ToString())).ToList();
         }
 
@@ -56,7 +56,7 @@ namespace DB
                 foreach (var name in names)
                 {
                     var query = new QueryDocument(new BsonDocument { { name, type } });
-                    var Root = Collections.Attributes.FindAs<BsonDocument>(query).ToList();
+                    var Root = DataBase.Attributes.FindAs<BsonDocument>(query).ToList();
 
                     list.AddRange(Root.Select(attr => int.Parse(attr["_id"].ToString())));
                 }
